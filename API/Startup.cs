c4a89872c3fbf130accc4093695adc6c746bcaf6
Damian <info@codenamics.pl>
2021-05-29 00:31:22 +0200

    cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -56,11 +56,11 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader()
-            .AllowCredentials()
-            .AllowAnyMethod()
-            .AllowCredentials()
-            .WithOrigins("https://localhost:4200"));
+            // app.UseCors(x => x.AllowAnyHeader()
+            // .AllowCredentials()
+            // .AllowAnyMethod()
+            // .AllowCredentials()
+            // .WithOrigins("https://localhost:4200"));
             app.UseAuthentication();
             app.UseAuthorization();
 
